c194eccbbf65a04f6c39b189561977daba2080bd
LEON <dlwotjr2@gmail.com>
2021-11-22 10:47:24 +0900

    2.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${props => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: tomato;
 `;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
 
 function App() {
   return (
   <Father>
-    <Box bgColor="teal" />
-    <Circle bgColor="gold" />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
+    <Input />
   </Father>
   )
 }
