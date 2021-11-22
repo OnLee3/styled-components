2dbb655a547a29f3c0a4bf0d9a0c0e1d747c22d9
LEON <dlwotjr2@gmail.com>
2021-11-22 11:05:12 +0900

    2.4 Animation and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
+const rotateAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    border-radius:100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 48px;
+    &:hover {
+        font-size: 72px;
+    }
+  }
 `;
 
 function App() {
   return (
-  <Father>
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-    <Input />
-  </Father>
+  <Wrapper>
+    <Box>
+      <span>😊</span>
+    </Box>
+  </Wrapper>
   )
 }
 
