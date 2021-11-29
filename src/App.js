aba12f2ee50b6a10e7c129b8f2c543524306da90
LEON <dlwotjr2@gmail.com>
2021-11-29 22:02:22 +0900

    2.6 Super Recap

--- src/App.js
+++ src/App.js
@@ -1,50 +1,26 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: red;
+  &:hover {
+      color: yellow;
+  }
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   justify-content: center;
   align-items: center;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform:rotate(0deg);
-    border-radius:0px;
+  ${Title}:hover {
+    font-size: 98px;
   }
-  50%{
-    border-radius:100px;
-  }
-  100% {
-    transform:rotate(360deg);
-    border-radius:0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 48px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content:center;
-  align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-        font-size: 96px;
-    }
 `;
 
 function App() {
   return (
   <Wrapper>
-    <Box>
-      <Emoji>😊</Emoji>
-    </Box>
-    <Emoji>🔥</Emoji>
+    <Title>Hello</Title>
   </Wrapper>
   )
 }
